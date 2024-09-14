718657acec8a06457fe87731a8b33801ac6e7e28
subastianhambali@gmail.com <subastianhambali@gmail.com>
2024-09-15 00:58:53 +0700

    merubah file

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "https://hambali-020608.github.io/website-youtube-downloader/"
+    base: "https://website-youtube-downloader.vercel.app/"
 })
