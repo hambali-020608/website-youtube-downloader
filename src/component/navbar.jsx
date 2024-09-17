058dae276f3b4a683176c33aa2d595e86c1f8d2a
subastianhambali@gmail.com <subastianhambali@gmail.com>
2024-09-17 22:11:21 +0700

    memperbaiki tampilan

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -6,7 +6,7 @@ function Navbar({children}){
         <>
       <nav className="navbar navbar-expand-lg bg-body-transparent" >
   <div className="container-fluid">
-    <a className="navbar-brand" href="#" style={{fontSize:"2rem", color:"white"}}>Profesor-Youtube</a>
+    <a className="navbar-brand" href="#" style={{fontSize:"1rem", color:"white"}}>Profesor-Youtube</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
