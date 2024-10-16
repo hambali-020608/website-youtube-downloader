01917df07058b404c0edb71d5f8335626ae95836
hambali <subastianhambali@gmail.com>
2024-10-16 07:44:00 +0700

    mengubah tampilan dan url api nya

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "https://website-youtube-downloader.vercel.app/",
+    base: "https://website-youtube-downloader.vercel.app",
     server:{
         proxy:{
             '/api': {
-        target: 'https://server-downloader-opamj92np-hambalis-projects-37a7e0e5.vercel.app',
+        target: 'https://server-downloader-1egvyoyzu-hambalis-projects-37a7e0e5.vercel.app/api/download',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
