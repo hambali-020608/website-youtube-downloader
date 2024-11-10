0b9b3ca35aa66b255e11afd2d111a9b2cab88c05
hambali <subastianhambali@gmail.com>
2024-11-10 12:36:11 +0700

    mengganti api

--- src/app/api/youtube/route.js
+++ src/app/api/youtube/route.js
@@ -16,18 +16,7 @@ export async function GET(request) {
     const url = searchParams.get('url')
   
     try {
-        const youtubeResponse = await fetch('https://shinoa.us.kg/api/download/ytdl', {
-            method: 'POST',
-            headers: {
-                'accept': '*/*',
-                'api_key': 'free',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ text: url })
-        });
-        if (!youtubeResponse.ok) {
-            throw new Error(`HTTP error! Status: ${youtubeResponse.status}`);
-        }
+        const info = await ytdl.getInfo(url)
 
           return NextResponse.json(info.formats)
           
