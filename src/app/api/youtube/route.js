a1c5b8aa8e6d0214c6160bd608a68eb8089ab1c8
hambali <subastianhambali@gmail.com>
2024-11-10 13:11:08 +0700

    api ke 2

--- src/app/api/youtube/route.js
+++ src/app/api/youtube/route.js
@@ -1,19 +1,35 @@
 import { NextResponse } from "next/server";
 import Download from "../../../function/download";
+import { YtdlCore } from '@ybd-project/ytdl-core/serverless';
+// JavaScript: const { YtdlCore } = require('@ybd-project/ytdl-core');
 
 
 export async function GET(request) {
     const searchParams = request.nextUrl.searchParams
     const url = searchParams.get('url')
-  
-    try {
-        const youtubeResponse = await Download('https://shinoa.us.kg/api/download/ytdl',url)
-        if (!youtubeResponse.ok) {
-            throw new Error(`HTTP error! Status: ${youtubeResponse.status}`);
-        }
+    
+    
+const ytdl = new YtdlCore({
+    // The options specified here will be the default values when functions such as getFullInfo are executed.
+});
 
-        const data = await youtubeResponse.json();
-        return NextResponse.json(data);
+// Download a video
+
+
+// Get video info
+
+
+
+try {
+        const data = await ytdl.getBasicInfo('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+        
+        // const youtubeResponse = await Download('https://shinoa.us.kg/api/download/ytdl',url)
+        // if (!youtubeResponse.ok) {
+        //     throw new Error(`HTTP error! Status: ${youtubeResponse.status}`);
+        // }
+
+        // const data = await youtubeResponse.json();
+        return NextResponse.json(data.formats);
     } catch (error) {
         return NextResponse.json(
             { message: "Error fetching YouTube data", error: error.message },
--- src/pages/downloadmp4.jsx
+++ src/pages/downloadmp4.jsx
@@ -9,6 +9,7 @@ import { useRouter } from "next/navigation"
 function Downloadmp4(){
     const router = useRouter()
     const [url,setUrl] = useState("")
+    const [datas,setDatas]= useState([])
     const [link,setLink]=useState(<p align="center" style={{position:'relative',top:'10rem'}} >silahkan masukan url terlebih dahulu lalu tunggu teks ini berubah jadi tombol download</p>)
     function handleinput(e){
         setUrl(e.target.value)
@@ -22,13 +23,14 @@ function Downloadmp4(){
         const youtube = await fetch(`/api/youtube?url=${url}`) 
         const data = await youtube.json()
         const video = data.data;
-
-        if (video.mp4) {
-            setLink(<a href={video.mp4} className='btn btn-danger ms-5' style={{ position: 'relative', top: '10rem' }} download>download</a>);
-        } else {
-            setLink(<p>Video tidak ditemukan.</p>);
+        setDatas(data)
+    
+    //     if (video.mp4) {
+    //         setLink(<a href={video.mp4} className='btn btn-danger ms-5' style={{ position: 'relative', top: '10rem' }} download>download</a>);
+    //     } else {
+    //         setLink(<p>Video tidak ditemukan.</p>);
         
-    }
+    // }
         } catch(error){
             console.error(error)
         }
@@ -47,7 +49,15 @@ function Downloadmp4(){
  <button className="btn btn-primary" type="button" onClick={download}>Search</button>
 
 </Layout> 
- 
+perubahan 2
+ {datas.map((video,i)=>{
+console.log(video.url)
+return(
+    <div key={i}>
+        <a href={video.url}>video</a>
+    </div>
+)
+ })}
 {link}
 
 </div>
