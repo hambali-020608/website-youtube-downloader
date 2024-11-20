bd4d94d0ed0c4474c1ce7ae79c24086d694908a7
hambali <subastianhambali@gmail.com>
2024-11-20 08:02:03 +0700

    mengganti favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,12 @@
+
+
 export default function RootLayout({ children }) {
     return (
       <html lang="en">
         <head>
+          <link rel="icon" href="/download.ico" sizes="any" />
           <title>Youtube Downloader</title>
-          <meta name="description" content="My App is a..." />
+          <meta name="no ads, easy to use, and friendly user interface" content="youtube downloader" />
         </head>
         <body>
           <div id="root">{children}</div>
