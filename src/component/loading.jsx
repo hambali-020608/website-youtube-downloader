a3031a2c3cfdce51bdca29f7dc4e705f65151bcd
hambali <subastianhambali@gmail.com>
2024-12-28 12:41:14 +0700

    add button

---
+++ src/component/loading.jsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+    return (
+        <div  className="d-flex align-items-center">
+  <strong role="status">Loading...</strong>
+  <div className="spinner-border ms-auto" aria-hidden="true"></div>
+</div>
+    )
+    
+}
